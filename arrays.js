92096653c25dae0c42b6bc40b289402b8004475b
Jaxon Hermansen <jaxherm35@gmail.comgit config --user.name Jaxon>
2022-04-14 12:34:20 -0600

    finished demo2

--- arrays.js
+++ arrays.js
@@ -1,4 +1,30 @@
-/*
-    In this file, we'll be continuing our story
-    with Jon being allowed to flee his fight with Jamie.
-*/
+let backpack = []
+
+backpack.push('hatchet', 'water')
+backpack.unshift('mac n cheese')
+backpack.push('sleeping bag')
+
+let belt = backpack.splice(1,1)
+
+backpack.pop()
+backpack.push('sleeping bag')
+
+let backpack2 = backpack.slice(1,3)
+
+backpack2.push('blanket', 'knife')
+
+
+// for(let i = 0; i < backpack.length; i++){
+//     console.log(backpack[i])
+// }
+
+// for(let i = 0; i <= backpack2.length - 1; i++){
+//     console.log(backpack2[i])
+// }
+
+for(let i = 0; i < 2; i++){
+    console.log(backpack[i])
+}
+for(let i =0; i < 2; i++){
+    console.log(backpack2[i])
+}
